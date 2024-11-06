4da6afaf0e44540490eac5d0f7066d8ce39142cf
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-06 15:37:28 +0100

    feat: fine esercizio 2 con aiuto da documentazione e hint

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,32 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e 
+restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/**
+ * Description placeholder
+ *
+ * @param {Array} namesArray
+ * @param {string} letters
+ * @returns {Array}
+ */
+
+function chooseByLetter(namesArray, letters) {
+    const result = []; // Array delle iniziali
+    for (let i = 0; i < namesArray.length; i++) {
+        if (namesArray[i][0] === letters) {
+            result.push(namesArray[1]);
+        }
+    }
+    return result;
+}
 
 
-// Invoca la funzione qui e stampa il risultato in console
-
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+const result = chooseByLetter(names, "L");
+console.log(result);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
