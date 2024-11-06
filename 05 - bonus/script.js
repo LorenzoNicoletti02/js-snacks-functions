54f692ce6c6b3069369f3a4dbeb0d2c02ba83ebc
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-06 17:53:35 +0100

    feat: fine bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -7,12 +7,26 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
-
 // Dichiara la funzione qui.
+function getCurrentTime(name) {
+    const time = new Date();
+    let saluto = ("");
 
+    if (time.getHours() <= 13) {
+        saluto = `Buongiorno ${name}`;
 
+    } else if (time.getHours() <= 17) {
+        saluto = `Buon pomeriggio ${name}`;
+
+    } else if (time.getHours() >= 17) {
+        saluto = `Buonasera ${name}`;
+    }
+    
+    return saluto;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+getCurrentTime(name);
+console.log(getCurrentTime(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
