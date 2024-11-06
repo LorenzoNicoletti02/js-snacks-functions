6801a7fa6c9744c213b31a6f9dd6d72ebf330150
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-11-06 16:03:06 +0100

    feat: terzo esercizio

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,24 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/**
+ * Description placeholder
+ *
+ * @returns {number}
+ */
 
-// Invoca la funzione qui e stampa il risultato in console
+function vocalsCount(parola) {
+    const vocals = "aeiou";
+    let count = 0;
 
+    for (let i = 0; i < parola.length; i++){
+        if (vocals.includes(parola[i])) {
+            count++;
+        }
+    }
+    return count;
+}
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(vocalsCount(word));
